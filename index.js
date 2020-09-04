7946ff9e43dc669f6620906bacec522990d52020
afolabiopakunle <opakunleafolabi@gmail.com>
2020-09-05 00:09:54 +0100

    code working, about to be modularized

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ let rect = {
     perimeter: (x, y) => (2 * (x + y)),
     area: (x, y) => (x * y)
 };
+
+let solveRect = function (x, y) {
+    if (x <= 0 || y <= 0) {
+        console.log("Error")
+    } else {
+        console.log("Perimeter is: " + rect.perimeter(x, y));
+        console.log("Area is: " + rect.area(x, y));
+    }
+}
+
+
+solveRect(45, 87)
